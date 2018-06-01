b34bf3441db65c4202569cba6ad8aeecf7dc5cbe
Xuan <xuanpl@MrXuandeMacBook-Pro.local>
2018-06-01 11:00:38 +0800

    forgot

---
+++ routes/monitoring.js
@@ -0,0 +1,23 @@
+// import {get} from '../src/service'
+var get = require('../src/service_req');
+
+var axios = require('axios');
+
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.post('/warningInfoUpdate', function(req, res, next) {
+	var projectCode = req.body.projectCode;
+  axios.get('https://xy-mind.com/').then(function(res){
+  })
+  // res.send('respond with a resource');
+  if (!projectCode) {
+	  res.status(500).json({ success: '参数无效' });
+	}else {
+		global.socket.emit(projectCode, { projectCode: projectCode, type: 'warning' });
+	  res.status(200).json({success: true, msg: '收到，发了就好小伙子。', data: null}  );
+	}
+});
+
+module.exports = router;
---
+++ src/service_req.js
@@ -0,0 +1,4 @@
+var axios = require('axios');
+var qs = require('qs');
+
+module.exports = [1,2,3]

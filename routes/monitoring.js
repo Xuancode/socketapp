28a88bad64a3c6991491628d5d88184878c6a22e
Xuan <xuanpl@MrXuandeMacBook-Pro.local>
2018-06-07 11:59:49 +0800

    test

--- routes/monitoring.js
+++ routes/monitoring.js
@@ -9,6 +9,7 @@ var router = express.Router();
 /* GET users listing. */
 router.post('/warningInfoUpdate', function(req, res, next) {
 	var projectCode = req.body.projectCode;
+  console.log(req.body)
   // res.send('respond with a resource');
   if (!projectCode) {
 	  res.status(500).json({ success: '参数无效' });
